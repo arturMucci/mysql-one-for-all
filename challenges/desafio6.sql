1c3c8c946ac51f296da8dfdd1f40f9d7f539a28e
arturMucci <artur.mucci@gmail.com>
2023-03-03 18:18:54 -0300

    requisito 6

---
+++ challenges/desafio6.sql
@@ -0,0 +1,9 @@
+SELECT
+	MIN(pl.valor) AS faturamento_minimo,
+  MAX(pl.valor) AS faturamento_maximo,
+  ROUND(AVG(pl.valor), 2) AS faturamento_medio,
+  SUM(pl.valor) AS faturamento_total
+FROM
+	planos AS pl
+INNER JOIN
+	users AS us ON pl.id = us.plano_id;
